7f98bae56f17c3b86b21f52180bdd7cd59ac9eba
fermmotta <35852594+fermmotta@users.noreply.github.com>
2018-03-15 12:34:17 -0400

    Update audio.js

--- audio.js
+++ audio.js
@@ -1,47 +1,70 @@
+const keys = document.querySelectorAll('.sound');
+//create an array off all of the nav elements.  If its in the header navigtion and its an <a/> we add it to instruments
+const instruments = document.querySelectorAll('header>nav>a');
+
+function clickSound(e) {
+  //e.target is the sound the user clicked on, dataset['sound'] looks for the value of data-sound in the element the user clicked
+  //summary:  soundKey, holds the name of the sound we want to play
+  let soundKey = e.target.dataset['sound'];
+  //audio uses querySelector to find the <audio> tag with the same data-sound as the link the user clicked on
+  let audio = document.querySelector(`audio[data-sound="${soundKey}"]`);
+  //now that we have the audio tag, we tell it to start playing from 0
+  audio.currentTime = 0;
+  audio.play();
+}
+//the swtichInstrument function waits for a click on a nav element
+function switchInstrument(e) {
+  //e.preventDefault() tells the page not to reload
+  e.preventDefault();
+  //we define inst1 and inst2 to represent they instrument1 and 2 sections from our page
+  let inst1 = document.querySelector('#instrument1');
+  let inst2 = document.querySelector('#instrument2');
+  let inst3 = document.querySelector('#instrument3');
+  //instrument holds the value of data-instrument from the link the user clicked on
+  let instrument = e.target.dataset['instrument'];
+  //if the user clicked on instrument1, we turn on instrument 1 and turn off instrument2
+  if (instrument == "instrument1") {
+    inst1.style.display = "flex";
+    inst2.style.display = "none";
+    //return tells the function to stop running
+    return;
+  }
+  //if the user didn't select instrument1, they end up here, where we turn off instrument1 and turn on instrument2
+  inst1.style.display = "none";
+  inst2.style.display = "flex";
+}
+
+//this command looks at the keys array.  It goes through each entry and adds a listener for click events
+//if a click event happens, it calls the function named clickSound
+keys.forEach(function(key) {
+  key.addEventListener("click", clickSound);
+});
+
+//this command looks at the instruments array(array that holds a tags in the nav)
+//we instruct each of the nav links to look out for clicks
+//if they get clicked on, they call switchInstrument
+instruments.forEach(function(instrument) {
+  instrument.addEventListener("click", switchInstrument);
+});
+
+
+
+
+
+
+
+
+
 let sect1 = "I am section1";
 let sect2 = "I am section2";
 let sect3 = "I am section3";
 
 let mySection = sect2;
 
-if(mySection==sect1){
+if (mySection == sect1) {
   console.log("turn on 1, turn off 2 and 3");
-}else if(mySection==sect2){
+} else if (mySection == sect2) {
   console.log("turn on 2, turn off 1 and 3");
-}else{
+} else {
   console.log("turn on 3, turn off 1 and 2");
 }
-
-<!--audio js-->
-
-const keys = document.querySelectorAll('.sound');
-const instruments = document.querySelectorAll('header>nav>a');
-
-function clickSound(e){
-  let soundKey = e.target.dataset['sound'];
-  let audio = document.querySelector(`audio[data-sound="${soundKey}"]`);
-  audio.currentTime = 0;
-  audio.play();
-}
-
-function switchInstrument(e){
-  e.preventDefault();
-  let inst1 = document.querySelector('#Brazil');
-  let inst2 = document.querySelector('#Italian');
-  let instrument = e.target.dataset['instrument'];
-  if(instrument=="instrument1"){
-    inst1.style.display = "flex";
-    inst2.style.display = "none";
-    return;
-  }
-  inst1.style.display = "none";
-  inst2.style.display = "flex";
-}
-
-keys.forEach(function(key){
-    key.addEventListener("click", clickSound);
-});
-
-instruments.forEach(function(instrument){
-  instrument.addEventListener("click", switchInstrument);
-});
